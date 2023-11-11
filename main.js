924e5d80fc7e3687c109ec7753eb586de90991e5
Vicky <vkrngn@gmail.com>
2023-11-11 19:03:32 +0530

    Project Completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // importing help function from commands folder
 let helpFunction = require("./commands/help");
 let organizeFunction = require("./commands/organize");
+let treeFunction = require("./commands/tree");
 
 let inputArr = process.argv.slice(2); // takes input and slice the first 2 keyword i.e (node main.js)
 let command = inputArr[0];
@@ -10,7 +11,7 @@ let path = inputArr[1];
 switch (command) {
   case "tree":
     // call tree function
-
+    treeFunction.tree(path);
     break;
   case "organize":
     // call organize function
